69118c63a0b6f1a9af2bf84cc7209ec2251ebd13
misterfarouk <faroukmuhammed84@gmail.com>
2019-06-24 16:09:59 +0100

    getting there

--- backend/data_retrieval.php
+++ backend/data_retrieval.php
@@ -2,16 +2,39 @@
     require_once('meetingscheduler.php');
 
     $sql = "SELECT * FROM staff_selection";
+    
     $results = mysqli_query( $connection, $sql);
+    $return_arr =  array();
 
     if( mysqli_num_rows($results) > 0 ){
         
         //output data
         while( $rows = mysqli_fetch_assoc($results) ){
-            echo $rows;
-        }
+            $return_arr[] = array( "id"=>$rows['id'], "staff_name"=>$rows['staff_name']);
+        };
+            $new_array[] = array("data"=>$return_arr);
+            echo json_encode($return_arr) ;
     }else{
         echo "data not found.";
     }
+
+
+    //meeting topic data retrieval
+    // $tpc = "SELECT * FROM meeting_topic";
+    
+    // $tpc_results =  mysqli_query( $connection, $tpc);
+    // $return_arr =  array();
+
+    // if( mysqli_num_rows($results) > 0 ){
+        
+    //     //output data
+    //     while( $rows = mysqli_fetch_assoc($results) ){
+    //         $return_arr[] = array( "id"=>$rows['id'], "topic"=>$rows['topic']);
+    //     };
+    //         $new_array[] = array("data"=>$return_arr);
+    //         echo json_encode($return_arr) ;       
+    // }else{
+    //     echo "data not found.";
+    // }
 
 ?>
--- js/main.js
+++ js/main.js
@@ -3,51 +3,39 @@ $(document).ready(function(){
     $.ajax({
         url: './backend/data_retrieval.php',
         type: 'get',
+        dataType: 'json',
         success: function(data){
+         
+            //staff selection
+            $.each(data, function(index, key){
 
-            console.log("Staff:"+data);
-
-            $.each(data, function(key, value)
-            {
-                $("#staff_selection").append('<option value=' + key + '>' + value + '</option>');
+                var option = new Option(key.staff_name, key.id);
+                console.log("fghj:"+ key.staff_name);
+                 $("#selection").append(option);
             });
-         }
+                //Change the text of the default "loading" option.
+                $('#staff_selection').text('Please select a staff');
+
+
+
+        //    //meeting topics     
+        //     console.log("Meeting Topic:"+data);
+        //     $.each(data, function(index, key){
+
+        //         var option = new Option(key.topic, key.id);
+        //         console.log("fghj:"+ key.topic);
+        //         $("#topic").append(option);
+        //     });
+        //             //Change the text of the default "loading" option.
+        //         $('#Meeting_topic').text('Please select a Meeting Topic');
+        
+        }
 
         });
-            // $.each(data, function(key, staffName){
-            //     var option = new Option(staffName, staffName);
-                
-            //     $(option).html(staffName);
             
-            //     $("#selection").append(option);
-            // });
-            //         //Change the text of the default "loading" option.
-            //     $('#staff_selection').text('Please select a staff');
-    
-   
 
-    // //Make an Ajax request to a PHP script called select_dropdown.php
-    //     //This will return the data that we can add to our Select element.
-    //     $.ajax({
-    //         url: './backend/topic.php',
-    //         type: 'get',
-    //         success: function(data){
     
-    //             //Log the data to the console so that
-    //             //you can get a better view of what the script is returning.
-    //             console.log("Meeting Topic:"+data);
-    
-    //             $.each(data, function(key, topicName){
-    //                 //Use the Option() constructor to create a new HTMLOptionElement.
-    //                 var option = new Option(topicName, topicName);
-    //                 //Convert the HTMLOptionElement into a JQuery object that can be used with the append method.
-    //                 $(option).html(topicName);
-    //                 //Append the option to our Select element.
-    //                 $("#topic").append(option);
-    //             });
-    //                     //Change the text of the default "loading" option.
-    //                 $('#Meeting_topic').text('Please select a Meeting Topic');
-    //         }
+
     //     });
 
     //     //Make an Ajax request to a PHP script called select_dropdown.php
