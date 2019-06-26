c5e1b2f4ce861b42bd0a4eb52ab6cecc9dfa952c
misterfarouk <faroukmuhammed84@gmail.com>
2019-06-26 10:24:08 +0100

    new files

--- backend/data_retrieval.php
+++ backend/data_retrieval.php
@@ -19,22 +19,4 @@
     }
 
 
-    //meeting topic data retrieval
-    // $tpc = "SELECT * FROM meeting_topic";
-    
-    // $tpc_results =  mysqli_query( $connection, $tpc);
-    // $return_arr =  array();
-
-    // if( mysqli_num_rows($results) > 0 ){
-        
-    //     //output data
-    //     while( $rows = mysqli_fetch_assoc($results) ){
-    //         $return_arr[] = array( "id"=>$rows['id'], "topic"=>$rows['topic']);
-    //     };
-    //         $new_array[] = array("data"=>$return_arr);
-    //         echo json_encode($return_arr) ;       
-    // }else{
-    //     echo "data not found.";
-    // }
-
 ?>
--- js/main.js
+++ js/main.js
@@ -16,49 +16,52 @@ $(document).ready(function(){
                 //Change the text of the default "loading" option.
                 $('#staff_selection').text('Please select a staff');
 
+            }
 
+    });
+    
+    $.ajax({
+        url: './backend/data_retrieval2.php',
+        type: 'get',
+        dataType: 'json',
+        success: function(data){
+         
+            //staff selection
+            $.each(data, function(index, key){
 
-        //    //meeting topics     
-        //     console.log("Meeting Topic:"+data);
-        //     $.each(data, function(index, key){
+                var option = new Option(key.topic, key.id);
+                console.log("fghj:"+ key.topic);
+                 $("#topic").append(option);
+            });
+                //Change the text of the default "loading" option.
+                $('#topic_selection').text('Please select a topic');
 
-        //         var option = new Option(key.topic, key.id);
-        //         console.log("fghj:"+ key.topic);
-        //         $("#topic").append(option);
-        //     });
-        //             //Change the text of the default "loading" option.
-        //         $('#Meeting_topic').text('Please select a Meeting Topic');
-        
-        }
+            }
 
-        });
-            
+    });
+
+    $.ajax({
+        url: './backend/data_retrieval3.php',
+        type: 'get',
+        dataType: 'json',
+        success: function(data){
+         
+            //staff selection
+            $.each(data, function(index, key){
+
+                var option = new Option(key.time, key.id);
+                console.log("fghj:"+ key.time);
+                 $("#time").append(option);
+            });
+                //Change the text of the default "loading" option.
+                $('#Meeting_time').text('Please select a meeting time');
+
+            }
+
+    });
+
 
     
 
-    //     });
-
-    //     //Make an Ajax request to a PHP script called select_dropdown.php
-    //     //This will return the data that we can add to our Select element.
-    // $.ajax({
-    //     url: './backend/time.php',
-    //     type: 'get',
-    //     success: function(data){
-
-    //         //Log the data to the console so that
-    //         //you can get a better view of what the script is returning.
-    //         console.log("Meeting Time:"+data);
-
-    //         $.each(data, function(key, meetingTime){
-    //             //Use the Option() constructor to create a new HTMLOptionElement.
-    //             var option = new Option(meetingTime, meetingTime);
-    //             //Convert the HTMLOptionElement into a JQuery object that can be used with the append method.
-    //             $(option).html(meetingTime);
-    //             //Append the option to our Select element.
-    //             $("#time").append(option);
-    //         });
-    //                 //Change the text of the default "loading" option.
-    //             $('#Meeting_time').text('Please select a Meeting Time');
-    //     }
-    // });
+    
 });
