b2945d8072014e66e3e2812388ff3dd008af7d88
misterfarouk <faroukmuhammed84@gmail.com>
2019-07-03 13:28:03 +0100

    wednesday

--- backend/insert.php
+++ backend/insert.php
@@ -2,34 +2,35 @@
 
 require_once('meetingscheduler.php');
 
-
-    $staff_name = $_POST['staff_name'];
-    $meeting_topic = $_POST['meeting_topic'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $staff_name = $_POST['staff_selection'];
+    $meeting_topic = $_POST['topic_selection'];
     $meeting_time = $_POST['meeting_time'];
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        if ($staff_name == null || $staff_name == "") {
-          $staff_nameErr = "Staff Name is required";
-        } else {
-          $staff_name = test_input($staff_name);
-        }
+        // if ($staff_name == null || $staff_name == "") {
+        //   $staff_nameErr = "Staff Name is required";
+        // } else {
+        //   $staff_name = test_input($staff_name);
+        // }
       
-        if ($meeting_topic == null || $meeting_topic == "") {
-            $meeting_topicErr = "meeting topic is required";
-          } else {
-            $meeting_topic = test_input($meeting_topic);
-          }
+        // if ($meeting_topic == null || $meeting_topic == "") {
+        //     $meeting_topicErr = "meeting topic is required";
+        //   } else {
+        //     $meeting_topic = test_input($meeting_topic);
+        //   }
       
-        if ($meeting_time == null || $meeting_time == "") {
-            $meeting_timeErr = "meeting time is required";
-          } else {
-            $meeting_time = test_input($meeting_time);
-        }
-        if (empty($staff_nameErr) && empty($meeting_topicErr) && empty($meeting_timeErr)) 
-        {
-            $sql = "INSERT INTO bookings (staff_name, meeting_topic, meeting_time)
-            VALUES ( '$staff_name', '$meeting_topic', '$meeting_time')";
+        // if ($meeting_time == null || $meeting_time == "") {
+        //     $meeting_timeErr = "meeting time is required";
+        //   } else {
+        //     $meeting_time = test_input($meeting_time);
+        // }
+        // if (empty($staff_nameErr) && empty($meeting_topicErr) && empty($meeting_timeErr)) 
+        // {
+            $sql = "INSERT INTO bookings ( name, email, staff_name, meeting_topic, meeting_time)
+            VALUES ( '$name', '$email' ,'$staff_name', '$meeting_topic', '$meeting_time')";
         
             if (mysqli_query($connection, $sql)) {
               http_response_code(200);
@@ -39,12 +40,11 @@ require_once('meetingscheduler.php');
               echo "Error: " . $sql . "<br>" . mysqli_error($connection);
             }
         }  else {
-          $arr = array('staff_name' => $staff_nameErr, 'meeting_topic' => $meeting_topicErr, 'meeting_time' => $meeting_timeErr, "status" => 400);
+          $arr = array( 'name' => $nameErr, 'email' => $emailErr, 'staff_name' => $staff_selectionErr, 'meeting_topic' => $topic_selectionErr, 'meeting_time' => $meeting_timeErr, "status" => 400);
           http_response_code(400);
           echo json_encode( $arr );
         }
           
-      };
 
 ?>
 
