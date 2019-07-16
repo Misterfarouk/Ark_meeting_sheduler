4141bc65099cae4167fc83eddc882898c7460db3
Misterfarouk <49267664+Misterfarouk@users.noreply.github.com>
2019-07-16 15:02:50 +0100

    UI

--- backend/insert.php
+++ backend/insert.php
@@ -4,7 +4,7 @@ require_once('meetingscheduler.php');
 
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $staff_name = $_POST['staff_selection'];
+    $staff_id = $_POST['staff_selection'];
     $meeting_topic = $_POST['topic_selection'];
     $meeting_time = $_POST['meeting_time'];
 
@@ -29,8 +29,8 @@ require_once('meetingscheduler.php');
         // }
         // if (empty($staff_nameErr) && empty($meeting_topicErr) && empty($meeting_timeErr)) 
         // {
-            $sql = "INSERT INTO bookings ( name, email, staff_name, meeting_topic, meeting_time)
-            VALUES ( '$name', '$email' ,'$staff_name', '$meeting_topic', '$meeting_time')";
+            $sql = "INSERT INTO bookings ( name, email, staff_id, meeting_topic, meeting_time)
+            VALUES ( '$name', '$email' ,'$staff_id', '$meeting_topic', '$meeting_time')";
         
             if (mysqli_query($connection, $sql)) {
               http_response_code(200);
@@ -40,7 +40,7 @@ require_once('meetingscheduler.php');
               echo "Error: " . $sql . "<br>" . mysqli_error($connection);
             }
         }  else {
-          $arr = array( 'name' => $nameErr, 'email' => $emailErr, 'staff_name' => $staff_selectionErr, 'meeting_topic' => $topic_selectionErr, 'meeting_time' => $meeting_timeErr, "status" => 400);
+          $arr = array( 'name' => $nameErr, 'email' => $emailErr, 'staff_id' => $staff_selectionErr, 'meeting_topic' => $topic_selectionErr, 'meeting_time' => $meeting_timeErr, "status" => 400);
           http_response_code(400);
           echo json_encode( $arr );
         }
